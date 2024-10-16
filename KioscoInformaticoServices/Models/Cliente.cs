ffb7e5f8fef3997bc0cb7acd72099facfcaf9962
Marcelo <marcelofabre71@gmail.com>
2024-10-16 18:08:14 -0300

    Refactorización y mejoras en la gestión de clientes
    Se han realizado los siguientes cambios:
    
    - Ajustes en el diseño del informe `ClientesReport.rdlc`.
    - Nueva entrada de mimetype en `ClientesView.resx`.
    - Validaciones de datos en el modelo `Cliente`.
    - Nuevas propiedades en `Resources.Designer.cs` y entradas en `Resources.resx`.
    - Lógica de selección de URL de API en `GenericService.cs`.
    - Eliminación del contenido de `AgregarCliente.razor` y `EditarCliente.razor`.
    - Actualización de navegación en `Clientes.razor`.
    - Inyección de dependencias en `Program.cs` para `HttpClient` y servicios.
    - Nueva página `EditarClienteABorrar.razor` con formulario de edición.
    - Nueva página `NuevoEditarCliente.razor` con formulario de agregar/editar cliente y lista de clientes.

--- KioscoInformaticoServices/Models/Cliente.cs
+++ KioscoInformaticoServices/Models/Cliente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace KioscoInformaticoServices.Models;
 
@@ -7,16 +8,20 @@ public partial class Cliente
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "El campo Nombre es obligatorio")]
     public string Nombre { get; set; } = null!;
 
+    [Required(ErrorMessage = "El campo Direccion es obligatorio")]
     public string Direccion { get; set; } = null!;
 
     public string Telefonos { get; set; } = null!;
 
     public DateTime FechaNacimiento { get; set; }
 
+    
     public int? LocalidadId { get; set; }
 
+    [Required(ErrorMessage = "El campo Localidad es obligatorio")]
     public virtual Localidad? Localidad { get; set; }
 
     public bool Eliminado { get; set; } = false;
