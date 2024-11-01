84519b58f6b891471f9dbefb1fecdb0ce1d00d13
Marcelo <marcelofabre71@gmail.com>
2024-11-01 16:08:49 -0300

    Añadir manejo de mensajes en KioscoShell
    Se ha añadido la referencia a `CommunityToolkit.Mvvm.Messaging` y `KioscoInformaticoApp.Class` en el archivo `KioscoShell.xaml.cs`.
    Se ha registrado un receptor de mensajes utilizando `WeakReferenceMessenger.Default.Register<Message>` en el constructor de `KioscoShell`.
    Se ha añadido el método `OnReceiveMessage` que maneja diferentes tipos de mensajes (`AbrirProducto`, `AbrirOfertas`, `AgregarProducto`, `EditarProducto`) y realiza acciones comentadas (navegación a diferentes vistas) en función del valor del mensaje recibido.

--- KioscoInformaticoApp/KioscoInformaticoApp/KioscoShell.xaml.cs
+++ KioscoInformaticoApp/KioscoInformaticoApp/KioscoShell.xaml.cs
@@ -1,10 +1,43 @@
-﻿namespace KioscoInformaticoApp
+﻿using CommunityToolkit.Mvvm.Messaging;
+using KioscoInformaticoApp.Class;
+
+namespace KioscoInformaticoApp
 {
     public partial class KioscoShell : Shell
     {
         public KioscoShell()
         {
             InitializeComponent();
+            WeakReferenceMessenger.Default.Register<Message>(this, (r, mensaje) =>
+            {
+                OnReceiveMessage(mensaje);
+            });
         }
+    }
+
+
+    private async void OnReceiveMessage(Message mensaje)
+    {
+        if (mensaje.Value == "AbrirProducto")
+        {
+            //await Navigation.PushAsync(new ProductosView());
+        }
+        if (mensaje.Value == "AbrirOfertas")
+        {
+            //await Navigation.PushAsync(new ProductosEnOfertaView());
+        }
+
+        if (mensaje.Value == "AgregarProducto")
+        {
+            //await Navigation.PushAsync(new AddEditProductoView(mensaje.ProductoAEditar));
+        }
+
+        if (mensaje.Value == "EditarProducto")
+        {
+            //await Navigation.PushAsync(new AddEditProductoView(mensaje.ProductoAEditar));
+        }
+
+   
+
     }
 }
