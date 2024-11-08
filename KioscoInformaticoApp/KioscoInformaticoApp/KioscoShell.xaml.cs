b7d07f0ef30d1e2265fdb728ed26bbbc79c8c473
Marcelo Fabre <marcelofabre71@gmail.com>
2024-11-08 12:20:52 -0300

    Rediseño de UI y cambio a API remota
    Se han comentado las líneas de código en `KioscoShell.xaml.cs` que registraban el uso de `WeakReferenceMessenger` para recibir mensajes y ejecutar acciones basadas en esos mensajes.
    
    En `AddEditProductoView.xaml`, `ProductosEnOfertaView.xaml` y `ProductosView.xaml`, se ha rediseñado la interfaz de usuario para mejorar la apariencia visual. Se han añadido `Frame` para bordes redondeados, ajustado los colores y tamaños de los textos y botones, y añadido un fondo principal en las vistas de productos.
    
    En `Resources.resx`, se ha cambiado el valor de la clave `Remoto` de `false` a `true`, indicando el uso de la URL de la API remota en lugar de la local.

--- KioscoInformaticoApp/KioscoInformaticoApp/KioscoShell.xaml.cs
+++ KioscoInformaticoApp/KioscoInformaticoApp/KioscoShell.xaml.cs
@@ -8,36 +8,36 @@ namespace KioscoInformaticoApp
         public KioscoShell()
         {
             InitializeComponent();
-            WeakReferenceMessenger.Default.Register<Message>(this, (r, mensaje) =>
-            {
-                OnReceiveMessage(mensaje);
-            });
+            //WeakReferenceMessenger.Default.Register<Message>(this, (r, mensaje) =>
+            //{
+            //    OnReceiveMessage(mensaje);
+            //});
         }
     }
 
 
-    private async void OnReceiveMessage(Message mensaje)
-    {
-        if (mensaje.Value == "AbrirProducto")
-        {
-            //await Navigation.PushAsync(new ProductosView());
-        }
-        if (mensaje.Value == "AbrirOfertas")
-        {
-            //await Navigation.PushAsync(new ProductosEnOfertaView());
-        }
+    //private async void OnReceiveMessage(Message mensaje)
+    //{
+    //    if (mensaje.Value == "AbrirProducto")
+    //    {
+    //        //await Navigation.PushAsync(new ProductosView());
+    //    }
+    //    if (mensaje.Value == "AbrirOfertas")
+    //    {
+    //        //await Navigation.PushAsync(new ProductosEnOfertaView());
+    //    }
 
-        if (mensaje.Value == "AgregarProducto")
-        {
-            //await Navigation.PushAsync(new AddEditProductoView(mensaje.ProductoAEditar));
-        }
+    //    if (mensaje.Value == "AgregarProducto")
+    //    {
+    //        //await Navigation.PushAsync(new AddEditProductoView(mensaje.ProductoAEditar));
+    //    }
 
-        if (mensaje.Value == "EditarProducto")
-        {
-            //await Navigation.PushAsync(new AddEditProductoView(mensaje.ProductoAEditar));
-        }
+    //    if (mensaje.Value == "EditarProducto")
+    //    {
+    //        //await Navigation.PushAsync(new AddEditProductoView(mensaje.ProductoAEditar));
+    //    }
 
    
 
-    }
+    //}
 }
